c555211704d8a6c02b6cf3205705540162b30fb1
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-06-15 10:38:16 +0200

    set up boilerplate

---
+++ config/db.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGODB_URI, {
+            dbName: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD,
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
