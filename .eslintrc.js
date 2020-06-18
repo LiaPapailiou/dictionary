bb60ad689f13d138fc4caa8f51e962cec70a015a
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-06-18 17:52:39 +0200

    fix linting errors & set production route for deployemnt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true
-    },
-    "plugins": [
-        "mocha"
-    ],
-    "parserOptions": {
-        "sourceType": "module",
-        "ecmaVersion": 2018
-    },
-    "extends": [
-        "airbnb-base"
-    ]
-}
+  env: {
+    node: true,
+    browser: true,
+  },
+  plugins: [
+    'mocha',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+  },
+};
--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const express = require('express');
-const connectDB = require('./config/db');
 const handlebars = require('express-handlebars');
+const path = require('path');
+const connectDB = require('./config/db');
 const routes = require('./routes/api/dictionary');
 
 const app = express();
 
 connectDB();
-
 app.use(express.static('public'));
 
 // Configure express-handlebars
 app.engine('.hbs', handlebars({
-    layoutsDir: `${__dirname}/views/layouts`,
-    extname: 'hbs',
-    defaultLayout: 'main',
-    partialsDir: `${__dirname}/views/partials`,
+  layoutsDir: `${__dirname}/views/layouts`,
+  extname: 'hbs',
+  defaultLayout: 'main',
+  partialsDir: `${__dirname}/views/partials`,
 }));
 app.set('view engine', '.hbs');
+app.set('views', path.resolve(__dirname, 'views'));
 
 // Middleware
 app.use(express.json({ extended: false }));
 
-
 app.use('/api/terms', routes);
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public'));
+  app.get('/', (req, res) => {
+    res.render('terms.hbs');
+  });
+}
 
 const port = process.env.PORT || 5000;
 
