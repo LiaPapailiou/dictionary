6cad8e3531a17dbb723f7ba423c0292d97009b93
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-06-19 11:01:43 +0200

    change render view

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/api/terms', routes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('public'));
   app.get('/', (req, res) => {
-    res.render('terms.hbs');
+    res.render('layouts/main.hbs');
   });
 }
 
